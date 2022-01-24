b842816967c87bd7c89b306075e24cda63019c62
thomasbititude <thomas@bititude.com>
2022-01-24 10:49:31 +0530

    passing data with event and property binding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,14 +7,17 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+ @Output() recipeWasSelected = new EventEmitter<Recipe>();
 recipes: Recipe[] = [
   new Recipe('A test Recipe','This is simply a test','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnogp9RuaF22YxGMm7T8UXSCI_IlYU_0NZ4A&usqp=CAU')
-,  new Recipe('A test Recipe','This is simply a test','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnogp9RuaF22YxGMm7T8UXSCI_IlYU_0NZ4A&usqp=CAU')
+,  new Recipe('Another test Recipe','This is simply a test','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnogp9RuaF22YxGMm7T8UXSCI_IlYU_0NZ4A&usqp=CAU')
 
 ];
   constructor() { }
 
   ngOnInit(): void {
   }
-
+onRecipeSelected(recipe:Recipe){
+this.recipeWasSelected.emit(recipe);
+}
 }
--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -2,7 +2,7 @@ export class Recipe{
   public name : string;
   public description : string;
   public imagepath : string;
-  
+
   constructor(name:string,desc:string,imagepath:string){
       this.name = name;
       this.description = desc;
