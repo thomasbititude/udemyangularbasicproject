f41d0296206adf0ac29bf8f63a20f27c683bf18f
thomasbititude <thomas@bititude.com>
2022-01-25 19:05:03 +0530

    adding ingredients to recipes

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,11 +1,20 @@
-export class Recipe{
-  public name : string;
-  public description : string;
-  public imagepath : string;
+import { Ingredient } from '../shared/ingredient.model';
 
-  constructor(name:string,desc:string,imagepath:string){
-      this.name = name;
-      this.description = desc;
-      this.imagepath = imagepath;
+export class Recipe {
+  public name: string;
+  public description: string;
+  public imagepath: string;
+  public ingredients: Ingredient[];
+
+  constructor(
+    name: string,
+    desc: string,
+    imagepath: string,
+    ingredients: Ingredient[]
+  ) {
+    this.name = name;
+    this.description = desc;
+    this.imagepath = imagepath;
+    this.ingredients = ingredients;
   }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,22 +1,25 @@
-import { EventEmitter } from "@angular/core";
-import { Recipe } from "./recipe.model";
+import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { Recipe } from './recipe.model';
 
 export class RecipeService {
-recipeSelected = new EventEmitter<Recipe>();
+  recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
     new Recipe(
-      'A test Recipe',
-      'This is simply a test',
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnogp9RuaF22YxGMm7T8UXSCI_IlYU_0NZ4A&usqp=CAU'
+      'Tasty Schnitzel',
+      'A super tasty Scnitzel',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnogp9RuaF22YxGMm7T8UXSCI_IlYU_0NZ4A&usqp=CAU',
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
     ),
     new Recipe(
-      'Another test Recipe',
-      'This is simply a test',
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnogp9RuaF22YxGMm7T8UXSCI_IlYU_0NZ4A&usqp=CAU'
+      'Burger',
+      'What else yuo want!',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnogp9RuaF22YxGMm7T8UXSCI_IlYU_0NZ4A&usqp=CAU',
+      [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
     ),
   ];
-  getRecipe(){
-      return this.recipes.slice();
+  getRecipe() {
+    return this.recipes.slice();
   }
 }
