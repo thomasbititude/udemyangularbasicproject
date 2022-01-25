288286ae00254be87780bbcfd3709bcbcae25bde
thomasbititude <thomas@bititude.com>
2022-01-25 17:27:11 +0530

    using services for pushing data from A to B

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -5,14 +5,17 @@ import { ShoppingListService } from './shopping-list.service';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent implements OnInit {
-ingredients :Ingredient[]=[];
-  constructor(private slService:ShoppingListService) { }
+  ingredients: Ingredient[] = [];
+
+  constructor(private slService: ShoppingListService) {}
 
   ngOnInit(): void {
     this.ingredients = this.slService.getIngredients();
+    this.slService.ingredientsChanged.subscribe((ingredients: Ingredient[]) => {
+      this.ingredients = ingredients;
+    });
   }
-
 }
