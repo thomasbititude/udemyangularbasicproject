456e0bd0bfc0297853c53b0a6694e9236abb0b36
thomasbititude <thomas@bititude.com>
2022-01-29 17:41:30 +0530

    added editing routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RecipeDetailsComponent } from './recipes/recipe-details/recipe-details.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
@@ -9,8 +10,9 @@ const appRoutes: Routes = [
   {path:'',redirectTo:'/recipes',pathMatch:'full'},
   {path: 'recipes',component: RecipesComponent ,children:[
     {path:'',component:RecipeStartComponent},
-    {path:':id',component:RecipeDetailsComponent}
-
+    {path:'new',component:RecipeEditComponent},
+    {path:':id',component:RecipeDetailsComponent},
+    {path:':id/edit',component:RecipeEditComponent}
   ]},
   {path:'shopping-list',component:ShoppingListComponent}
   
