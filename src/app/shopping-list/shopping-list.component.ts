8b5f64044fc5750932618029702eee1bf6074bb5
thomasbititude <thomas@bititude.com>
2022-01-31 11:31:32 +0530

    observsbles added and subject in shopping list component

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
 
@@ -7,15 +8,20 @@ import { ShoppingListService } from './shopping-list.service';
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css'],
 })
-export class ShoppingListComponent implements OnInit {
-  ingredients: Ingredient[] = [];
+export class ShoppingListComponent implements OnInit ,OnDestroy {
+  ingredients: Ingredient[]=[];
+  private subscription:Subscription;
 
-  constructor(private slService: ShoppingListService) {}
+  constructor(private slService: ShoppingListService
+    ) {}
 
   ngOnInit(): void {
     this.ingredients = this.slService.getIngredients();
-    this.slService.ingredientsChanged.subscribe((ingredients: Ingredient[]) => {
+    this.subscription =  this.slService.ingredientsChanged.subscribe((ingredients: Ingredient[]) => {
       this.ingredients = ingredients;
     });
+  }
+  ngOnDestroy(): void {
+   this.subscription.unsubscribe();   
   }
 }
