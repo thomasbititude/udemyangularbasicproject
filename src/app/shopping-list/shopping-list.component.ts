12cde5dbb8eb389316461272b7febda7e2655ab5
thomasbititude <thomas@bititude.com>
2022-02-01 18:58:52 +0530

    updating existing items in shopping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -21,6 +21,10 @@ export class ShoppingListComponent implements OnInit ,OnDestroy {
       this.ingredients = ingredients;
     });
   }
+  onEditItem(index:number){
+this.slService.startedEditing.next(index);
+  }
+
   ngOnDestroy(): void {
    this.subscription.unsubscribe();   
   }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -3,6 +3,7 @@ import { Ingredient } from '../shared/ingredient.model';
 
 export class ShoppingListService {
   ingredientsChanged = new Subject<Ingredient[]>();
+  startedEditing = new Subject<number>();
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
@@ -11,14 +12,22 @@ export class ShoppingListService {
   getIngredients() {
     return this.ingredients.slice();
   }
+  getIngredient(index: number) {
+    return this.ingredients[index];
+  }
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
     this.ingredientsChanged.next(this.ingredients.slice());
   }
 
-  addIngredients(ingredients:Ingredient[]){
-this.ingredients.push(...ingredients);
-this.ingredientsChanged.next(this.ingredients.slice());
+  addIngredients(ingredients: Ingredient[]) {
+    this.ingredients.push(...ingredients);
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+
+  updateIngredient(index:number,newIngredient:Ingredient){
+    this.ingredients[index] = newIngredient;
+this.ingredientsChanged.next(this.ingredients.slice())
   }
 }
