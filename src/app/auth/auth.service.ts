625fe485fc101204af92c176d9f8c00c2c22f0f2
thomasbititude <thomas@bititude.com>
2022-02-05 15:39:52 +0530

    sending login requests

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,39 +1,54 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { catchError } from "rxjs";
-import { throwError} from 'rxjs'
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs';
+import { throwError } from 'rxjs';
 
-interface AuthResponseData{
-    kind:string;
-    idToken:string;
-    email:string;
-    refreshToken:string;
-    expiresIn:string;
-    localId:string;
+export interface AuthResponseData {
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered ?: boolean;
 }
 
-@Injectable({providedIn:'root'})
-export class AuthService{
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+  constructor(private http: HttpClient) {}
 
-constructor(private http:HttpClient){}
+  signup(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCJO7bakvrBp4GRYr0tBBUc_--OTsgtdSo',
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(
+        catchError((errorRes) => {
+          let errorMessage = 'An unknown error occured!';
+          if (!errorRes.error || !errorRes.error.error) {
+            return throwError(errorMessage);
+          }
+          switch (errorRes.error.error.message) {
+            case 'EMAIL_EXISTS':
+              errorMessage = 'This email exists already';
+          }
+          return throwError(errorMessage);
+        })
+      );
+  }
 
-signup(email: string,password: string){
-return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCJO7bakvrBp4GRYr0tBBUc_--OTsgtdSo',
-{
-    email: email,
-    password: password,
-    returnSecureToken:true
+login(email:string,password:string){
+    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCJO7bakvrBp4GRYr0tBBUc_--OTsgtdSo',
+    {
+        email: email,
+        password: password,
+        returnSecureToken: true,   
+    })
 }
-).pipe(catchError(errorRes =>{
-    let errorMessage = 'An unknown error occured!';
-    if(!errorRes.error || !errorRes.error.error){
-        return throwError(errorMessage);
-    }
-    switch(errorRes.error.error.message){
-        case 'EMAIL_EXISTS':
-            errorMessage = 'This email exists already'
-    }
-    return throwError(errorMessage);
-}));
-}
+
 }
